e20695ff1f6914e6ddb69ee0a01b61b76a34312c
Anatolij Grigorjev <grigorjev.anatolij@gmail.com>
2023-02-04 19:12:20 +0200

    add missing scrabble scoring logic

--- scrabble-score/scrabble_score.rb
+++ scrabble-score/scrabble_score.rb
@@ -10,10 +10,10 @@ class Scrabble
     }.reduce(Hash::new, :merge!)
   end
 
-  # flatten the original scoring table to be in form
-  # letter => score for quick access
   public
 
+  # flatten the original scoring table to be in form
+  # letter => score for quick access
   SCORING_TABLE = flatten({
     ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"] => 1,
     ["D", "G"] => 2,
@@ -23,4 +23,21 @@ class Scrabble
     ["J", "X"] => 8,
     ["Q", "Z"] => 10,
   }).freeze
+
+  def initialize(word_string)
+    @scrabble_word = word_string.nil? ? "" : word_string
+  end
+
+  def score
+    @scrabble_word
+      .upcase
+      .chars
+      .map { |character| SCORING_TABLE[character] }
+      .select { |score| !!score }
+      .reduce(0, :+)
+  end
+
+  def self.score(scrabble_word)
+    Scrabble::new(scrabble_word).score
+  end
 end
