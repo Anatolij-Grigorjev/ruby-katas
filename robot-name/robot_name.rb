3270f2aa34e0cacd9da6c19970e0a0883da5db1f
Anatolij Grigorjev <grigorjev.anatolij@gmail.com>
2023-02-06 21:31:49 +0200

    logic for generating robot names

--- robot-name/robot_name.rb
+++ robot-name/robot_name.rb
@@ -1,11 +1,50 @@
+require "set"
+
 class Robot
+  @@known_names = Set::new
+  @robot_name = ""
+
+  def self.forget()
+    @@known_names.clear()
+  end
+
+  def initialize()
+    reset()
+  end
+
+  def name
+    @robot_name
+  end
+
+  def reset()
+    @robot_name = generate_random_name()
+  end
+
   private
 
-  @@known_names = []
+  def self.rand_digit()
+    rand(0..9)
+  end
 
-  public
+  def self.rand_char()
+    rand("A".bytes.first.."Z".bytes.first).chr
+  end
 
-  def self.forget
-    @@known_names = []
+  def self.generate_new_name()
+    [
+      rand_char(),
+      rand_char(),
+      rand_digit(),
+      rand_digit(),
+      rand_digit(),
+    ].join
+  end
+
+  def generate_random_name()
+    potential_name = Robot.generate_new_name()
+    while @@known_names.add?(potential_name).nil?
+      potential_name = Robot.generate_new_name()
+    end
+    potential_name
   end
 end
