892fffce5d75911d5c2f0efbe98d1e9af978e28f
Anatolij Grigorjev <grigorjev.anatolij@gmail.com>
2023-02-08 20:48:28 +0200

    add missing logic for high-score tests

--- high-scores/high_scores.rb
+++ high-scores/high_scores.rb
@@ -1,11 +1,33 @@
 class HighScores
+  @scores = []
 
-  def initialize(arg)
+  def initialize(scores)
+    @scores = scores
   end
 
   def scores
+    @scores
   end
 
   def latest
+    @scores.last
+  end
+
+  def personal_best
+    top_n_scores(1)
+  end
+
+  def personal_top_three
+    top_n_scores(3)
+  end
+
+  def latest_is_personal_best?
+    personal_best == latest
+  end
+
+  private
+
+  def top_n_scores(n)
+    if n > 1 then @scores.max(n) else @scores.max end
   end
 end
